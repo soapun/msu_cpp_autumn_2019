c2fd99e95f11ac304ae406c145b7b7da7482d95e
soapun <leha5617441@gmail.com>
2019-10-12 11:55:06 +0300

    minor fixes

--- 01/test.py
+++ 01/test.py
@@ -1,13 +1,21 @@
 import subprocess
-
+flag = True
 
 def check(expr, result):
     out = subprocess.run(['./calc', expr], stdout=subprocess.PIPE)
     if out.stdout.decode('ascii') != result:
         print('error {0} != {1}, got {2}'.format(expr, result, out.stdout))
-    else:
-        print('ok')
+        flag = False
 
+
+
+check('hello there', 'String is not an expression.')
+check('(2', 'String is not an expression.')
+check('2*2(', 'String is not an expression.')
+check('2**2', 'String is not an expression.')
+check('2++2', 'String is not an expression.')
+check('+2', 'String is not an expression.')
+check('2 -= 2', 'String is not an expression.')
 
 check('2', '2')
 check('2 + 2', '4')
@@ -28,6 +36,8 @@ check('2 * -3', '-6')
 check('10 / 2', '5')
 check('10 / 2 / 2', '2')
 check('10 / -2', '-5')
-check('10 / -2 / 2', '-3')
+check('10 / -2 / 2', '-2')
 
-check('10 * -39 / 3 * 532 - -----666 / 101 + -1337 - 322 * 10----   ----123 ', '-73587')
+check('10 * -39 / 3 * 532 - -----666 / 101 + -1337 - 322 * 10----   ----123 ', '-73588')
+if flag:
+    print('All tests passed.')
