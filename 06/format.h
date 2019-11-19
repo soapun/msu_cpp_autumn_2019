b525274275a30099c1b5b17541ee935b741f1bf8
soapun <leha5617441@gmail.com>
2019-11-19 18:34:40 +0300

    redesigned

--- 06/format.h
+++ 06/format.h
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
-
 using namespace std;
 
 template <class T>
@@ -11,40 +10,27 @@ string to_string(const T &t) {
     return ss.str();
 }
 
-string format(const string &s) {
-    return s;
-}
-
 template <class ...Args>
 string format(const string &s, Args&&... args) {
-    vector<string> input {to_string(std::forward<Args>(args))...};
+    vector<string> input {to_string(forward<Args>(args))...};
     stringstream res;
-    size_t arg_idx;
-    size_t end = -1;
-    size_t b_sym, e_sym;
-    bool flag = false;
-    while ((b_sym = s.find("{", end + 1)) != string::npos) {
-        flag ^= true;
-        e_sym = s.find("}", end + 1);
-        if (e_sym < b_sym || e_sym == string::npos) {
+    for (size_t i = 0; i < s.size(); ++i) {
+        if (s[i] == '}')
             throw runtime_error("Error");
+        if (s[i] == '{') {
+            size_t arg_idx, end;
+            try {
+                arg_idx = stoull(s.substr(++i), &end);
+            } catch (logic_error &e) {
+                throw runtime_error("Error");
+            }
+            i += end;
+            if (arg_idx >= input.size() || s[i] != '}')
+                throw runtime_error("Error");
+            res << input[arg_idx];
+        } else {
+            res << s[i];
         }
-        flag ^= true;
-        size_t beg = end + 1;
-        try {
-            arg_idx = stoull(s.substr(b_sym + 1) , &end);
-            end += b_sym + 1;
-        }
-        catch (logic_error e) {
-            throw runtime_error("Error");
-        }
-        if (s[end] != '}' || arg_idx >= input.size()) {
-            throw runtime_error("Error");
-        }
-        res << s.substr(beg, b_sym - beg) << input[arg_idx];
     }
-    flag ^= (s.find("}", end + 1) != string::npos);
-    if (flag)
-        throw runtime_error("Error");
     return res.str();
 }
